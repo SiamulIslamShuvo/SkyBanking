53aefde65ed871dd689c8bfe2b8d8bf538beb66b
SiamulIslamShuvo <your.email@example.com>
2025-10-07 22:13:31 +0600

    login js added and linked with main

---
+++ script/login.js
@@ -0,0 +1,17 @@
+document
+  .getElementById('login-btn')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+    const acc = document.getElementById('acc-no').value;
+    const pin = document.getElementById('pin-no').value;
+    const convertedPin = parseInt(pin);
+    if (acc.length === 13) {
+      if (convertedPin === 1234) {
+        window.location.href = 'main.html';
+      } else {
+        alert('PIN No is invalid');
+      }
+    } else {
+      alert('Account No is invalid');
+    }
+  });
