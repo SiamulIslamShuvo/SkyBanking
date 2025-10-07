f798e9c5f2a32d574915cef0910a8431c6fc6368
SiamulIslamShuvo <your.email@example.com>
2025-10-08 00:57:43 +0600

    feature Section and part of add money added

--- script/login.js
+++ script/login.js
@@ -7,7 +7,7 @@ document
     const convertedPin = parseInt(pin);
     if (acc.length === 13) {
       if (convertedPin === 1234) {
-        window.location.href = 'main.html';
+        window.location.href = 'Home.html';
       } else {
         alert('PIN No is invalid');
       }
